0ee016656097d6aed5626e2b1f890f96f71644c2
wmh <wangminghui@zyxtravel.com>
2020-07-29 15:23:43 +0800

    delete code

--- springboot-web/src/main/java/wang/mh/controller/HelloController.java
+++ springboot-web/src/main/java/wang/mh/controller/HelloController.java
@@ -1,30 +1,20 @@
 package wang.mh.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import wang.mh.exception.TestException;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class HelloController {
 
     private List<int[]> list = new ArrayList<>();
 
     @RequestMapping(value = "/hello")
-    @ResponseBody
     public Map hello(String name){
 
         System.out.println(name);
